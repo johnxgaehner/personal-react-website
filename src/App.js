150ba294fb4beb7d06b440fc782d5747979f810d
John Goehner <john.goehner@gmx.de>
2021-07-21 16:57:48 +0200

    Add basic navigation + switch including routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { Switch, Route, NavLink } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <NavLink exact to="/">
+            JLG
+          </NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Switch>
+          <Route path="/about">About</Route>
+          <Route path="/contact">Contact</Route>
+          <Route path="/">JLG</Route>
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
