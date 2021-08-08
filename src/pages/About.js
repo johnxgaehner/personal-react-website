ed1fedcbf9525f057b02f7f782ba80ed1876b3e9
John Goehner <john.goehner@gmx.de>
2021-08-08 23:49:50 +0200

    Center contact

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,18 +7,6 @@ export default function About() {
       bootcamp, learning all the tools & technologies to be able to translate
       ideas into code. Eager to span a broad range, I have a knack on design and
       interaction.
-      {/* <div className="about-skillset">
-        <p>current skill set.</p>
-        <ul className="about-skillset__list">
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>GIT</li>
-          <li>JAVASCRIPT</li>
-          <li>REACT</li>
-          <li>PATIENCE</li>
-          <li>MOTIVATION</li>
-        </ul>
-      </div> */}
     </section>
   );
 }
